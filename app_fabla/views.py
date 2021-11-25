8528c242840724bae8579a1010aee3ac0f47c089
0290141 <tky2002015@stu.o-hara.ac.jp>
2021-11-25 16:10:09 +0900

    11/25

--- app_fabla/views.py
+++ app_fabla/views.py
@@ -1,6 +1,15 @@
 from django.views import generic
 
+from .models import *
+
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
 # Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+    def get_queryset(self):
+        return super().get_queryset()
