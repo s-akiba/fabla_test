05408150fc52f7a52fe6acd275e8ed7c8703cfea
0290141 <tky2002015@stu.o-hara.ac.jp>
2021-11-25 15:41:17 +0900

    11/25

--- app_fabla/urls.py
+++ app_fabla/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'app_fabla'
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
+    path('post-list/',views.PostListView.as_view(), name="post_list"),
 ]
