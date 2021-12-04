fd5dbeea680214cb2fca6922e5646a9b2e48ca32
github <94416297+MurphyYES@users.noreply.github.com>
2021-12-04 22:54:00 +0900

    12/04 settings

--- pj_fabla/settings.py
+++ pj_fabla/settings.py
@@ -188,7 +188,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
 
 # ログイン/ログアウト後の遷移先を設定
-#LOGIN_REDIRECT_URL = 'diary:diary_list'
+LOGIN_REDIRECT_URL = 'app_fabla:post_list'
 #ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
 # ログアウトリンクのクリック一発でログアウトする設定
@@ -211,3 +211,9 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD = 'user_name'
+
+ACCOUNT_FORMS = { "signup":"accounts.forms.SignupForm"}
+ 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
