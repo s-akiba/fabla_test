32a04ec440885f51bfba84f1fe2008509bb250dc
ssttootky2002118 <tky2002118@stu.o-hara.ac.jp>
2022-01-24 15:19:45 +0900

    01241519

--- app_fabla/forms.py
+++ app_fabla/forms.py
@@ -1,10 +1,23 @@
 from django import forms
-from .models import Post
+from .models import Post,PostReport
+
 
 class AppFablaCreateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'photo', 'category_no')
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = PostReport
+        fields = ('report_reason',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
