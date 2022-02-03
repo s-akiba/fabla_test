c611dbd554c7d52a85d858ed6aece159e6e10a94
github <94416297+MurphyYES@users.noreply.github.com>
2022-02-03 15:32:52 +0900

    保永

--- pj_fabla/urls.py
+++ pj_fabla/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from . import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_fabla.urls')),
     path('accounts/',include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
